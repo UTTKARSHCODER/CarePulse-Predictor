1c24ddce48a87651e159ac9b2b9ea53ed1eab61f
UTTKARSHCODER <uttkarshyadav702@gmail.com>
2026-06-25 13:02:21 +0530

    Updated the fetch from localhost to live render server

--- health-insurance-ui/src/App.jsx
+++ health-insurance-ui/src/App.jsx
@@ -43,7 +43,7 @@ export default function App() {
     };
 
     try {
-      const response = await fetch('http://127.0.0.1:5000/predict', {
+      const response = await fetch('https://carepulse-backend-r1j0.onrender.com/predict', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
